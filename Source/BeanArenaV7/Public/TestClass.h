d6e4ab7b0ffcb8679cfec8410d741f4dc78cee04
Scott Meekin <147707872+Smeekin21@users.noreply.github.com>
2025-09-30 12:50:09 +0100

    Initial Commit

---
+++ Source/BeanArenaV7/Public/TestClass.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "TestClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEANARENAV7_API UTestClass : public UObject
+{
+	GENERATED_BODY()
+	
+};
